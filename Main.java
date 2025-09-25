5fae771c49161f0f66845d6914f39d7b765821cf
David Yin <29613693+E137299@users.noreply.github.com>
2025-09-25 08:50:37 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,18 +1,16 @@
 public class Main{
   public static void main(String[] args){
+      long start = System.nanoTime();   
+
+    //Run your method here
 
     
-  }
-
-    // Method to measure how long a Runnable takes to execute
-  public static void measureTime(Runnable codeBlock) {
-      long start = System.nanoTime();   // record start time
-      codeBlock.run();                  // run the code
-      long end = System.nanoTime();     // record end time
+      long end = System.nanoTime();     
 
       long duration = end - start; // time in nanoseconds
       double millis = duration / 1_000_000.0;
 
       System.out.println("Execution time: " + millis + " ms");
+    
   }
 }
